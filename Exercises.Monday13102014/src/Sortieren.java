e506a2fe85f000016d8f3835b1299e5141757b52
McRadio <steinhagen.steffen@gmail.com>
2014-10-15 13:25:50 +0200

    ExerciseMonday speichert das Array sortiert und unsortiert

--- Exercises.Monday13102014/src/Sortieren.java
+++ Exercises.Monday13102014/src/Sortieren.java
@@ -1,3 +1,6 @@
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.PrintWriter;
 import java.util.Arrays;
 
 /**
@@ -8,18 +11,37 @@ public class Sortieren {
 
 	/**
 	 * @param args
+	 * @throws IOException 
 	 */
 	
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-		int i=9;
-		int [] a = new int [i+1];
-		for (; i>=0; i--) {
-			a[i] = myRandom();
+	public static void main(String[] args) throws IOException {
+		PrintWriter ausgabe = null;
+		PrintWriter ausgabeSorted = null;
+		try{
+			// TODO Auto-generated method stub
+			int i=9;
+			int [] a = new int [i+1];
+			for (; i>=0; i--) {
+				a[i] = myRandom();
+			}
+			System.out.println(	Arrays.toString(a));
+
+			ausgabe = new  PrintWriter(new FileWriter("unsorted.txt"));
+			ausgabe.println(Arrays.toString(a));
+			Bubblesort.sortieren(a);
+			ausgabeSorted = new  PrintWriter(new FileWriter("sorted.txt"));
+			System.out.println(	Arrays.toString(a));
+			ausgabeSorted.println(Arrays.toString(a));
+				
+		}finally
+		{
+			if(ausgabe != null){
+				ausgabe.close();
+			}
+			if(ausgabeSorted != null){
+				ausgabeSorted.close();
+			}
 		}
-		System.out.println(	Arrays.toString(a));
-		Bubblesort.sortieren(a);
-		System.out.println(	Arrays.toString(a));
 	}
 
 	public static int myRandom() {
